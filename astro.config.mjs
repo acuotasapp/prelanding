6dfba3fb98c93cadc05546f665826183240ba7ee
Julián Cipagauta <julian.cipa96@gmail.com>
2025-05-17 04:33:20 -0500

    chore(prepare for deploy to sandbox (github pages)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://acuotasapp.github.io/',
+  base: 'prelanding',
 });
